bc423f7be3645dcbb55ce686b8c3dad8bb6ecc9d
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-01-16 15:22:00 -0500

    Create stats.cpp

---
+++ stats.cpp
@@ -0,0 +1,11 @@
+// Include header files
+#include "stats.h" 
+
+// Implement the functions average, min, max, median
+double average(int x, int y, int z) {
+  double ret;
+
+  ret = (x + y + z) / 3.0;
+
+  return ret;
+}
