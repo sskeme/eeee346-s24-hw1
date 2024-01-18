7b344210bd57565d1ea9d23e2b68837f83dca4e8
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-01-18 15:34:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include "stats.h"
 #include <iostream>
 
-/*
- *  Main function
- */
 int main() {
   int x;
   int y;
   int z;
 
-  // Set the precision for cout
+  // set the precision for cout
   std::cout.precision(2);
 
-  // Get three integers
+  // get three integers
   std::cin >> x >> y >> z;
 
-  // Output Average (Show in fixed point)
+  // output Average (show in fixed point)
   std::cout << "Average: " << std::fixed << average(x, y, z) << std::endl;
 
-  // Output Median, Min, Max
+  // output Median, Min, Max
 
   return 0;
 }
